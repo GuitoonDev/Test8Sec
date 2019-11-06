8b7f4456758fe69ff496e5bd0a98827acbe17902
gvergeot <vergeot.gaetan@gmail.com>
2019-11-06 18:18:35 +0100

    -Rework Folder Organisation

---
+++ Assets/Scripts/PlaneMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Renderer))]
+public class PlaneMovement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 1f;
+
+    private Renderer planeRenderer;
+
+    private void Start() {
+        planeRenderer = transform.GetComponent<Renderer>();
+    }
+
+    private void Update() {
+        Vector2 offset = planeRenderer.material.GetTextureOffset("_BaseMap");
+        offset -= Vector2.up * moveSpeed * Time.deltaTime;
+        planeRenderer.material.SetTextureOffset("_BaseMap", offset);
+    }
+}
